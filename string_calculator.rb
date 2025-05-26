d49c7852ef6c8885d2e3078f9ead0de129861b63
Parth Bharadiya <parthbharadiya007@gmail.com>
2025-05-26 08:50:07 +0530

    Refactor: extract parsing and error handling in private methods

--- string_calculator.rb
+++ string_calculator.rb
@@ -2,6 +2,16 @@ class StringCalculator
   def self.add(input)
     return 0 if input == ''
 
+    numbers = extract_numbers(input)
+    # Calling with ! because the execution will stop from here if any negative number(s)
+    check_for_negatives!(numbers)
+
+    numbers.sum
+  end
+
+  private
+
+  def self.extract_numbers(input)
     # Default delimiter
     delimiter = /[\n,]/
     # handle custom delimiter e.g //;\n1;2
@@ -10,12 +20,13 @@ class StringCalculator
       custom_delimiter = delimiter_line[2..]
       delimiter = /[#{Regexp.escape(custom_delimiter)}\n,]/
     end
-    numbers = input.split(delimiter).map(&:to_i)
+    input.split(delimiter).map(&:to_i)
+  end
 
+  def self.check_for_negatives!(numbers)
     negatives = numbers.select { |x| x < 0 }
     if negatives.any?
       raise "negative numbers not allowed #{negatives.join(',')}"
     end
-    numbers.sum
   end
 end
